a99fb18ffa040d06eb6dfcaa8fa828a5c7730023
Piret Saar <piret.saar.003@khk.ee>
2022-02-03 10:50:18 +0200

    Created form request responce with API data

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ const express = require('express')
 const app = express()
 const path = require('path')
 const fetch = require('node-fetch')
+const bodyParser = require('body-parser');
 
+app.use(bodyParser.urlencoded({extended:true}));
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, 'views'))
 
@@ -24,6 +26,24 @@ app.get('/', function (req, res) {
                 temp: temp
         })
     })
+})
+
+app.post('/', function (req, res) {
+    let city= req.body.cityname
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}`)
+        .then((responce) => {
+            return responce.json()
+        })
+        .then((data) => {
+            let description = data.weather[0].description
+            let city = data.name
+            let temp = Math.round(parseFloat(data.main.temp) -273.15)
+            res.render('index', {
+                description: description,
+                city: city,
+                temp: temp
+            })
+        })
 })
 
 app.listen(3000)
